fd7e98202f15e82b997d1ec74ed24c6f654e2b23
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-03-05 13:43:17 +0100

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <div className='px-20 border border-black'>
+        <button className='px-4 py-2 rounded-lg bg-slate-300'>Generate</button>
+    </div>
+  )
+}
